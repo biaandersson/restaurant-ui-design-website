75533c0a945dc4bcd2d29d801fae2e66b5acee76
Tobias Bia Andersson <tobias.andersson@comeon.com>
2022-01-29 23:26:02 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Restaurant UX Design Website
+
+This example is a Fully Responsive Restaurant Website with Modern UI and UX in
+ReactJS.
+
+## Preview
+
+Preview the example live on [StackBlitz](http://stackblitz.com/):
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/vercel/next.js/tree/canary/examples/with-tailwindcss)
+
+## Deploy your own
+
+Deploy the example
+using [Vercel](https://vercel.com?utm_source=github&utm_medium=readme&utm_campaign=next-example):
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https://github.com/vercel/next.js/tree/canary/examples/with-tailwindcss&project-name=with-tailwindcss&repository-name=with-tailwindcss)
+
+## How to use
+
+Execute [`create-react-app`](https://create-react-app.dev/docs/getting-started/)
+with [npm](https://docs.npmjs.com/cli/init)
+or [Yarn](https://yarnpkg.com/lang/en/docs/cli/create/) to bootstrap the
+example:
+
+```bash
+npx create-react-app app-name
+# or
+yarn create react-app app-name
+```
+
+Deploy it to the cloud
+with [Vercel](https://vercel.com/new?utm_source=github&utm_medium=readme&utm_campaign=next-example) ([Documentation](https://nextjs.org/docs/deployment))
+.
---
+++ src/container/Gallery/Gallery.jsx
@@ -0,0 +1,70 @@
+import React, { useRef, useState } from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from 'react-icons/bs';
+import './Gallery.css';
+
+const image = [
+  images.gallery01,
+  images.gallery02,
+  images.gallery03,
+  images.gallery04,
+];
+
+const Gallery = () => {
+  const scrollRef = useRef(null);
+
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+
+    if (direction === 'left') {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  return (
+    <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
+        <SubHeading title='Instagram' />
+        <h1 className='headtext__cormorant'>Photo Gallery</h1>
+        <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat
+          mattis ipsum turpis elit elit scelerisque egestas mu.
+        </p>
+        <button type='button' className='custom__button'>
+          View More
+        </button>
+      </div>
+      <div className='app__gallery-images'>
+        <div className='app__gallery-images_container' ref={scrollRef}>
+          {image.map((galleryImg, index) => (
+            <div
+              className='app__gallery-images_card flex__center'
+              key={`gallery__image-${index + 1}`}>
+              <img src={galleryImg} alt='gallery image' />
+              <BsInstagram className='gallery__image-icon' />
+            </div>
+          ))}
+        </div>
+        <div className='app__gallery-images_arrow'>
+          <BsArrowLeftShort
+            className='gallery__arrow-icon'
+            onClick={() => scroll('left')}
+          />
+          <BsArrowRightShort
+            className='gallery__arrow-icon'
+            onClick={() => scroll('right')}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
