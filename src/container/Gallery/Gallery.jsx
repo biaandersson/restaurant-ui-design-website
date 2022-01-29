1465813742f5a8536b5b88875b1e6e069bb81d69
Tobias Bia Andersson <tobias.andersson@comeon.com>
2022-01-29 23:31:01 +0100

    removed unused imports

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import {
